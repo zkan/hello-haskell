8c2051cf528ca6dd26c33f6d3d7a1723ebc06d7e
zkan <zkan.cs@gmail.com>
2021-01-10 14:47:59 +0700

    Fix fibo 0 :bear:

--- f.hs
+++ f.hs
@@ -9,6 +9,6 @@ factorial 0 = 1
 factorial x = x * factorial (x - 1)
 
 fibonacci :: Int -> Int
-fibonacci 0 = 1
+fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci x = fibonacci (x - 1) + fibonacci (x - 2)

e8f3d443f5b57063780d1e20a0108c0467900e47
zkan <zkan.cs@gmail.com>
2021-01-10 15:07:46 +0700

    Use guard for giving a grade :bear:

--- f.hs
+++ f.hs
@@ -21,3 +21,10 @@ countElem [] = "empty"
 countElem (_:[]) = "one"
 countElem (_:_:[]) = "two"
 countElem _ = "many"
+
+grade :: Int -> Char
+grade score
+  | score > 90 = 'A'
+  | score > 80 = 'B'
+  | score > 70 = 'C'
+  | otherwise = 'F'

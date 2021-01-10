171ab66252c247b9bd8551aa8b5c0de3cba10eee
zkan <zkan.cs@gmail.com>
2021-01-10 15:05:29 +0700

    Count elements in list :bear:

--- f.hs
+++ f.hs
@@ -12,3 +12,12 @@ fibonacci :: Int -> Int
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci x = fibonacci (x - 1) + fibonacci (x - 2)
+
+add2Tuples2 :: (Int, Int) -> (Int, Int) -> (Int, Int)
+add2Tuples2 a b = (fst a + fst b, snd a + snd b)
+
+countElem :: [a] -> String
+countElem [] = "empty"
+countElem (_:[]) = "one"
+countElem (_:_:[]) = "two"
+countElem _ = "many"

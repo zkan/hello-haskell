b980d92ff05bb22d5a98e696ce167cebb2116853
zkan <zkan.cs@gmail.com>
2021-01-10 14:38:42 +0700

    Implement factorial :bear:

--- f.hs
+++ f.hs
@@ -4,3 +4,8 @@ manutd_legend 7 = "Eric Cantona"
 manutd_legend 10 = "Wayne Rooney"
 manutd_legend 11 = "Ryan Giggs"
 manutd_legend _ = "No Legend"
+
+factorial :: Int -> Int
+
+factorial 0 = 1
+factorial x = x * factorial (x - 1)

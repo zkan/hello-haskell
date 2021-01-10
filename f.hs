b521d0584f8529e5adfa3f63a9cc173c6c479b5c
zkan <zkan.cs@gmail.com>
2021-01-10 14:45:04 +0700

    Adjust spacing :;bear:

--- f.hs
+++ f.hs
@@ -1,17 +1,14 @@
 manutd_legend :: Int -> String
-
 manutd_legend 7 = "Eric Cantona"
 manutd_legend 10 = "Wayne Rooney"
 manutd_legend 11 = "Ryan Giggs"
 manutd_legend _ = "No Legend"
 
 factorial :: Int -> Int
-
 factorial 0 = 1
 factorial x = x * factorial (x - 1)
 
 fibonacci :: Int -> Int
-
 fibonacci 0 = 1
 fibonacci 1 = 1
 fibonacci x = fibonacci (x - 1) + fibonacci (x - 2)
